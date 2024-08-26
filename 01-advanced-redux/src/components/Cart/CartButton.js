6b04aae2aa43e8aebc6dfbdef823a185a5a4464a
Oliver Then <oli171dr@gmail.com>
2024-08-26 17:05:17 -0400

    Toggle Cart Feature implemented

--- 01-advanced-redux/src/components/Cart/CartButton.js
+++ 01-advanced-redux/src/components/Cart/CartButton.js
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { cartActions } from '../../store';
+
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+	const dispatch = useDispatch();
+
+	const cart = useSelector(state => state.cartItems);
+	
+
+	const toggleCartHandler = () => {
+		dispatch(cartActions.toggleCart());
+	};
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cart.length}</span>
     </button>
   );
 };
