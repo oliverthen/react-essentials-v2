83f1da27381081624e2bfb4dfca5c042c8df9f1d
Oliver Then <oli171dr@gmail.com>
2024-08-28 06:24:32 -0400

    Using action creators to send and fetch data from backend

--- 01-advanced-redux/src/App.js
+++ 01-advanced-redux/src/App.js
@@ -3,9 +3,9 @@ import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { fetchCartData, sendCartData } from './store/cart-actions';
 import { useEffect } from 'react';
 
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
 
 let isInitial = true;
@@ -17,41 +17,20 @@ function App() {
 	const notification = useSelector((state) => state.ui.notification);
 
 	useEffect(() => {
-		const sendCartData = async () => {
-			dispatch(uiActions.showNotification({
-				status: 'pending',
-				title: 'Sending...',
-				message: 'Sending cart data!',
-			}));
-			const response = await fetch('https://reduxpractice-34bed-default-rtdb.firebaseio.com/cart.json', {
-			method: 'PUT',
-			body: JSON.stringify(cart),
-			});
+		dispatch(fetchCartData());
+	}, [dispatch]);
 
-			if (!response.ok) {
-				throw new Error('Sending cart data failed.');
-			}
-
-			dispatch(uiActions.showNotification({
-				status: 'success',
-				title: 'Success!',
-				message: 'Send cart data successfully!',
-			}));
-
-		};
-
+	useEffect(() => {
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
 
-		sendCartData().catch(error => {
-			dispatch(uiActions.showNotification({
-				status: 'error',
-				title: 'Error!',
-				message: 'Sending cart data failed!',
-			}));
-		});
+		if (cart.changed) {
+			dispatch(sendCartData(cart));
+		}
+
+		
 		
 	}, [cart, dispatch]);
 
