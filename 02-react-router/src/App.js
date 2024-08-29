429b534d7dad09809aa2496bce6fe1929899764c
Oliver Then <oli171dr@gmail.com>
2024-08-29 07:01:52 -0400

    Incoporate Routes, Links, and Navigation Bar

--- 02-react-router/src/App.js
+++ 02-react-router/src/App.js
@@ -1,5 +1,31 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from './pages/Home';
+import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error';
+
+// const routeDefinitions = createRoutesFromElements(
+// 	<Route>
+// 		<Route path='/' element={<HomePage />} />
+// 		<Route path='/products' element={<ProductsPage />} />
+// 	</Route>
+// );
+
+const router = createBrowserRouter([
+	{ path: '/', 
+		element: <RootLayout />,
+		errorElement: <ErrorPage />, 
+		children: [
+		{ path: '/', element: <HomePage /> },
+		{ path: '/products', element: <ProductsPage /> },
+		],
+	},
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
