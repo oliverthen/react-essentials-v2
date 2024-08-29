6c6b53b8e914078bcd1d2758b195df9242d50944
Oliver Then <oli171dr@gmail.com>
2024-08-29 13:24:04 -0400

    Finished Basic React Router Project

--- 02-react-router/src/App.js
+++ 02-react-router/src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 // const routeDefinitions = createRoutesFromElements(
 // 	<Route>
@@ -16,8 +17,9 @@ const router = createBrowserRouter([
 		element: <RootLayout />,
 		errorElement: <ErrorPage />, 
 		children: [
-		{ path: '/', element: <HomePage /> },
+		{ index: true, element: <HomePage /> },
 		{ path: '/products', element: <ProductsPage /> },
+		{ path: '/products/:productId', element: <ProductDetailPage /> },
 		],
 	},
 ]);
